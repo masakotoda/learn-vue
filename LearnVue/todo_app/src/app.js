695caceb97288790fc5d259c8bfd1541cf04466f
Masako Toda <todamasako0421@hotmail.com>
2017-06-25 14:54:05 -0700

    todo application ver 3

--- LearnVue/todo_app/src/app.js
+++ LearnVue/todo_app/src/app.js
@@ -1,38 +1,64 @@
 var app = new Vue({
     el: '#app',
     data: {
-        todo: null,
         message: 'Hello Vueeeee!',
+        newContent: null,
+        editingContent: null,
         todos: [
             {
                 content: 'Item 1',
-                finished: false
+                finished: false,
+                isEditing: false
             },
             {
                 content: 'Item 2',
-                finished: false
+                finished: false,
+                isEditing: false
             }
         ]
     },
     methods: {
-        add(todo) {
-            if (!this.todoIsValid)
-            {
+        add() {
+            if (!this.isNewContentValid)
                 return;
-            }
 
-            console.log('adding ' + todo.content);
-            this.todos.push({ content: todo, finished: false });
-            this.todo = null;
+            console.log('adding ' + this.newContent);
+            this.todos.push({
+                content: this.newContent,
+                finished: false,
+                isEditing: false
+            });
+            this.newContent = null;
+        },
+        edit(todo) {
+            this.editingContent = todo.content;
+            todo.isEditing = true;
         },
         remove(todo) {
             console.log('removing ' + todo.content);
             this.todos = this.todos.filter(item => item !== todo);
+        },
+        update(todo) {
+            if (!this.isEditingContentValid)
+                return;
+            todo.content = this.editingContent;
+            this.editingContent = null;
+            todo.isEditing = false;
+        },
+        cancel(todo) {
+            this.editingContent = null;
+            todo.isEditing = false;
         }
     },
     computed: {
-        todoIsValid() {
-            return !!this.todo;
+        isNewContentValid() {
+            return !!this.newContent;
+        },
+        isEditingContentValid() {
+            return this.editingContent;
+        },
+        isEditingAny() {
+            return this.todos.filter(todo => todo.isEditing).length > 0;
         }
     }
 })
