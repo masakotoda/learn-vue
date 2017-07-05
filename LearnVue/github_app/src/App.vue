54e4d7133bfdaf4ec68fad8be0c2fd61b1a8ff9d
Masako Toda <todamasako0421@hotmail.com>
2017-07-04 20:52:14 -0700

    github application ver 6 - using axios

--- LearnVue/github_app/src/App.vue
+++ LearnVue/github_app/src/App.vue
@@ -1,5 +1,6 @@
-<template>
+﻿<template>
   <div>
+    <pre>{{ user }}</pre>
     <router-view></router-view>
   </div>
 </template>
@@ -9,5 +10,22 @@
   export default {
     name: 'GitHubApplication',
     router: myrouter,
+
+    data() {
+      return {
+        user: null,
+      };
+    },
+
+    methods: {
+      fetchUser(username) {
+        this.$http.get(`https://api.github.com/users/${username}`)
+        .then((res) => { this.user = res.data; });
+      }
+    },
+
+    mounted() {
+      this.fetchUser('masakotoda');
+    }
   }
 </script>
--- LearnVue/github_app/src/bootstrap.js
+++ LearnVue/github_app/src/bootstrap.js
@@ -2,6 +2,15 @@
 
 Vue.config.debug = process.env.NODE_ENV !== 'production';
 
+import Axios from 'axios';
+
+Axios.defaults.headers.common.Accept = 'application/vnd.github.v3+json';
+
+// It's same thing as:
+// Vue.prototype.$http = Axios;
+// But it's safer this way:
+Object.defineProperty(Vue.prototype, '$http', { get() { return Axios;} });
+
 import VueRouter from 'vue-router';
 import myroutes from './routes';
 
