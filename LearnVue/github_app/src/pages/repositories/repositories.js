e1d0fd411ecf45046afae3d386a902c3ae388153
Masako Toda <todamasako0421@hotmail.com>
2017-07-08 16:01:44 -0700

    github application ver 17 - added sorting

--- LearnVue/github_app/src/pages/repositories/repositories.js
+++ LearnVue/github_app/src/pages/repositories/repositories.js
@@ -1,3 +1,21 @@
 ﻿export default {
-//
+  computed: {
+    repos() {
+      return this.$root.repos;
+    }
+  },
+
+  methods: {
+    sortRepos(sorting) {
+      this.repos.sort((a, b) => {
+        console.log(sorting);
+        if (a[sorting.name] < b[sorting.name])
+          return sorting.type == "asc" ? 1 : -1;
+        else if (a[sorting.name] > b[sorting.name])
+          return sorting.type == "asc" ? -1 : 1;
+        else
+          return 0;
+      });
+    }
+  }
 };
