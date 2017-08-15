1cd4ac2ea2df4a3dd34057b5fc8c021b8aa656f8
Masako Toda <todamasako0421@hotmail.com>
2017-08-14 22:01:33 -0700

    chat application ver 4 - implement server

---
+++ chat_app/server/src/main.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 3000;
+
+io.on('connection', function(socket) {
+    socket.on('enterRoom', function(room) {
+        socket.join(room);
+    });
+    socket.on('leaveRoom', function(room) {
+        socket.leave(room);
+    });
+    socket.on('newMessage', function(message) {
+        // don't need to understand this yet.
+        Object.keys(socket.rooms).forEach(function (room) {
+            io.sockets.in(room).emit('newMessage', message);
+        });
+    });
+});
+
+http.listen(port, function() {
+    console.log('Listening on *:' + port);
+});
